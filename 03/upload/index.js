a2381e1919dd83f2eb85efa0a87863d8871e89d5
Diamondsiron <273699943@com>
2019-11-20 15:58:30 +0800

    'upload'

--- 03/upload/index.js
+++ 03/upload/index.js
@@ -3,54 +3,48 @@ const fs = require('fs')
 const path = require('path')
 const chunk = []
 let size = 0
-const serve = http.createServer((req, res) => {
-    const {pathname} = require('url').parse(req.url)
-    console.log(pathname,'pathname')
+const server = http.createServer((request, response) => {
+    const { pathname } = require('url').parse(request.url)
+
     if (pathname === '/upload') {
-        console.log('upload...')
-        
-        const filename = req.headers['filename'] ? req.headers['file-name'] : 'abc.png'
-        const outputFile = path.resolve(__dirname, filename)
-        console.log(outputFile,'outputFile')
+        console.log('upload....')
+        const fileName = request.headers['file-name'] ? request.headers['file-name'] : 'abc.png'
+        const outputFile = path.resolve(__dirname, fileName)
         const fis = fs.createWriteStream(outputFile)
 
-        // //buffer写入 写入快 占运行内存
-        // req.on('data', data => {
+        // Buffer connect
+        // request.on('data',data => {
         //     chunk.push(data)
-        //     size += data.length;
-        //     console.log('data',data,size)
+        //     size += data.length
+        //     console.log('data:',data ,size)
         // })
-
-        // req.on('end',()=>{
-        //     console.log('...end')
-        //     const buffer = Buffer.concat(chunk, size)
-        //     size = 0;
+        // request.on('end',() => {
+        //     console.log('end...')
+        //     const buffer = Buffer.concat(chunk,size)
+        //     size = 0
         //     fs.writeFileSync(outputFile,buffer)
-        //     res.end()
+        //     response.end()
         // })
 
-        // //流时间写入
-        // req.on('data',data=>{
-        //     console.log('data',data)
+        // 流事件写入
+        // request.on('data', data => {
+        //     console.log('data:',data)
         //     fis.write(data)
         // })
-
-        // req.on('end',()=>{
+        // request.on('end', () => {
         //     fis.end()
-        //     res.end()
+        //     response.end()
         // })
 
-        req.pipe(fis)
-        res.end()
 
+        request.pipe(fis)
+        response.end()
 
     } else {
         const filename = pathname === '/' ? 'index.html' : pathname.substring(1)
-        //const filename = 'index.html'
-
-        var type = (function(_type) {
-            switch(_type){
-                case 'html' :
+        var type = (function (_type) {
+            switch (_type) { // 扩展名
+                case 'html':
                 case 'htm': return 'text/html charset=UTF-8'
                 case 'js': return 'application/javascript charset=UTF-8'
                 case 'css': return 'text/css charset=UTF-8'
@@ -58,19 +52,19 @@ const serve = http.createServer((req, res) => {
                 case 'manifest': return 'text/cache-manifest charset=UTF-8'
                 default: return 'application/octet-stream'
             }
-        }(filename.substring(filename.indexOf('.')+1)))
-        // const content = fs.readFileSync(filePath)
-        // ctx.body = content
-        fs.readFile(filename, function(err, content) {
+        }(filename.substring(filename.lastIndexOf('.') + 1)))
+        // 异步读取文件,并将内容作为单独的数据块传回给回调函数
+        // 对于确实很大的文件,使用API fs.createReadStream()更好
+        fs.readFile(filename, function (err, content) {
             if (err) { // 如果由于某些原因无法读取文件
-                res.writeHead(404, { 'Content-type': 'text/plain charset=UTF-8' })
-                res.write(err.message)
+                response.writeHead(404, { 'Content-type': 'text/plain charset=UTF-8' })
+                response.write(err.message)
             } else { // 否则读取文件成功
-                res.writeHead(200, { 'Content-type': type })
-                res.write(content) // 把文件内容作为响应主体
+                response.writeHead(200, { 'Content-type': type })
+                response.write(content) // 把文件内容作为响应主体
             }
-            res.end()
+            response.end()
         })
     }
 })
-serve.listen(3000)
+server.listen(3000)
