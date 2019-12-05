09268af3bd232a6529b18967fb2a5c6891ecb275
Diamondsiron <273699943@com>
2019-12-05 13:53:33 +0800

    'ts'

---
+++ 09/ts-decorate/src/index.ts
@@ -0,0 +1,96 @@
+// 类装饰器
+function anotationClass(id){
+    console.log('类装饰器来了', id);
+    return (target) => console.log('类装饰器', id);
+}
+// 方法装饰器
+function anotationMethods(id){
+    console.log('方法装饰器来了', id);
+    return (target, property, descriptor) => console.log('方法装饰器', id);
+}
+
+@anotationClass(1)
+@anotationClass(2)
+class Example {
+    @anotationMethods(1)
+    @anotationMethods(2)
+    method(){}
+}
+
+// 日志应用和切面实现
+console.log('日志应用和切面实现.....')
+function log(target, name, descriptor) {
+    console.log(target, name, descriptor)
+    var oldValue = descriptor.value;
+    //修改入参
+    descriptor.value = function () {
+        console.log(`Calling "${name}" with`, arguments);
+        return oldValue.apply(null, [1,3]);
+    }
+    return descriptor;
+}
+class Maths {
+    @log
+    add(a, b) {
+        console.log(a,b)
+        return a + b;
+    }
+}
+const math = new Maths()
+math.add(2, 4)
+
+let someValue: any = "this is a string";
+
+let strLength: number = (someValue as string).length;
+console.log(strLength,'strLength')
+//接口
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+  
+let myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+
+//泛型 不确定是什么类型 使用的时候再把类型穿进去
+function identity<T>(arg: T): T {
+    return arg;
+}
+let output = identity<string>("myString"); 
+console.log(output,'output')
+
+//代理模式
+import "reflect-metadata";
+
+class Point {
+    x: number;
+    y: number;
+}
+
+class Line {
+    private _p0: Point;
+    private _p1: Point;
+
+    @validate
+    set p0(value: Point) { this._p0 = value; }
+    get p0() { return this._p0; }
+
+    @validate
+    set p1(value: Point) { this._p1 = value; }
+    get p1() { return this._p1; }
+}
+
+function validate<T>(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<T>) {
+    let set = descriptor.set;
+    descriptor.set = function (value: T) {
+        let type = Reflect.getMetadata("design:type", target, propertyKey);
+        if (!(value instanceof type)) {
+            throw new TypeError("Invalid type.");
+        }
+        set(value);
+    }
+}
+
